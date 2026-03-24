f19bc0d731fc356bb4d83bdfb56ed96356c7ddb2
Joshua Hernandez <joshuahernandezalmaraz@gmail.com>
2026-03-23 21:13:05 -0600

    Initial commit

---
+++ minimalAPI/MinimalAPILearn/Program.cs
@@ -0,0 +1,22 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapOpenApi();
+app.MapScalarApiReference();
+
+app.UseHttpsRedirection();
+
+app.MapGet("/hello", () =>
+{
+    return "Hello World!";
+});
+
+app.Run();
